76aef86365274d3eccdb976a5badd6e3faa189b6
Jayaram J S <89035356+jayaramjs@users.noreply.github.com>
2026-04-09 18:27:12 +0530

    Add string compression functionality
    Implemented string compression algorithm to reduce consecutive character sequences.

--- src/StringCompression.java
+++ src/StringCompression.java
@@ -1,9 +1,45 @@
+import java.util.Scanner;
+
 public class StringCompression {
     
-        // TODO: Read the string
-        // TODO: Iterate through the string to count consecutive characters
-        // TODO: Append char and count to a StringBuilder
-        // TODO: Compare lengths of original vs compressed
-        // TODO: Print the shorter one (or compressed if equal length, depending on strict interpretation. Usually original if compressed is not strictly smaller, but prompt says "if compressed is longer")
-  
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Read the string
+        String str = sc.nextLine();
+
+        // Edge case: empty string
+        if (str.length() == 0) {
+            System.out.println("");
+            return;
+        }
+
+        StringBuilder compressed = new StringBuilder();
+
+        // Iterate through the string
+        int count = 1;
+
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == str.charAt(i - 1)) {
+                count++;
+            } else {
+                compressed.append(str.charAt(i - 1));
+                compressed.append(count);
+                count = 1;
+            }
+        }
+
+        // Append last character group
+        compressed.append(str.charAt(str.length() - 1));
+        compressed.append(count);
+
+        // Compare lengths and print result
+        if (compressed.length() < str.length()) {
+            System.out.println(compressed.toString());
+        } else {
+            System.out.println(str);
+        }
+
+        sc.close();
+    }
 }
