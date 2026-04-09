7b286ae19c5dcbe1107190c6573e1b2aa6bd19a6
Jayaram J S <89035356+jayaramjs@users.noreply.github.com>
2026-04-09 18:28:24 +0530

    Refactor StringCompression to improve variable names

--- src/StringCompression.java
+++ src/StringCompression.java
@@ -1,43 +1,43 @@
 import java.util.Scanner;
 
 public class StringCompression {
-    
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Read the string
-        String str = sc.nextLine();
+        // Read input string
+        String S = sc.nextLine();
 
-        // Edge case: empty string
-        if (str.length() == 0) {
+        // Edge case
+        if (S.length() == 0) {
             System.out.println("");
             return;
         }
 
         StringBuilder compressed = new StringBuilder();
 
-        // Iterate through the string
         int count = 1;
 
-        for (int i = 1; i < str.length(); i++) {
-            if (str.charAt(i) == str.charAt(i - 1)) {
+        // Traverse string
+        for (int i = 1; i < S.length(); i++) {
+            if (S.charAt(i) == S.charAt(i - 1)) {
                 count++;
             } else {
-                compressed.append(str.charAt(i - 1));
+                compressed.append(S.charAt(i - 1));
                 compressed.append(count);
                 count = 1;
             }
         }
 
-        // Append last character group
-        compressed.append(str.charAt(str.length() - 1));
+        // Add last character group
+        compressed.append(S.charAt(S.length() - 1));
         compressed.append(count);
 
-        // Compare lengths and print result
-        if (compressed.length() < str.length()) {
+        // Compare lengths
+        if (compressed.length() < S.length()) {
             System.out.println(compressed.toString());
         } else {
-            System.out.println(str);
+            System.out.println(S);
         }
 
         sc.close();
